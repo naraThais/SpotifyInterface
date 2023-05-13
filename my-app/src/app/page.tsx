e8e39cb4a6bc6baa02dd72a6dbe5e956b2590f0a
naraThais <Thaisna46@gmail.com>
2023-05-13 11:09:05 -0300

    Menu and Playlists

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,17 +1,45 @@
-import Image from "next/image";
+import {Home as HomeIcon, Search, Library} from 'lucide-react'
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
+
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-            <a href="">Home</a>
 
-            <a href="">Search</a>
+        <div className='flex itens-center gap-2'>
+         <div className='w-3 h-3 bg-red-500 rounded-full'/>
+         <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+         <div className='w-3 h-3 bg-green-500 rounded-full'/>
+        </div>
 
-            <a href="">Your Library</a>
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+
+            <a href="" className='flex items-center gap-3  text-sm  font-semibold text-zinc-200'>
+              <Search />
+              Search
+              </a>
+
+            <a href="" className='flex items-center gap-3  text-sm  font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+              </a>
           </nav>
+
+        <nav className='mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-2'>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+          <a href='text-sm text-zinc-400'>Playlist</a>
+        </nav>
+
         </aside>
 
         <main className="flex-1 p-6">main</main>
@@ -20,6 +48,7 @@ export default function Home() {
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
         footer
       </footer>
+
     </div>
   );
 }
